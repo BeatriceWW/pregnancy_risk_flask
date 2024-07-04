5e8a39196b980334fb9322652f8e251cc492a1f6
BeatriceWW <bwwaihenya@gmail.com>
2024-07-04 14:09:21 +0300

    Remove merge conflict markers and clean up the code

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-from app import app
+
+from pregriskpredapp import app
 
 if __name__ == "__main__":
-=======
-from app import app
-
-if __name__ == "__main__":
->>>>>>> 9b208022b11a1b2d0205c2b736dceb2ebb6a9901
     app.run()
